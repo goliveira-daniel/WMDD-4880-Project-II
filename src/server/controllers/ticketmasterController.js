6a46260e070ce1fbd3ce23b7eae38f186f0d81cf
goliveira-daniel <goliveira.daniel@gmail.com>
2017-10-23 13:14:34 -0700

    Updated getData function to return events array correctly

--- src/server/controllers/ticketmasterController.js
+++ src/server/controllers/ticketmasterController.js
@@ -1,27 +1,31 @@
 const model = require('../models/ticketmaster')
+const Obstruct = require('obstruction')
 
 const startHour = 'T00:00:00Z'
 const endHour = 'T23:59:59Z'
 
 function getData(req, res) {
     model({ startDateTime: req.query.startDateTime + startHour, endDateTime: req.query.endDateTime + endHour})
-    // let p = Promise.resolve(Data({ startDateTime: req.query.startDateTime + startHour, endDateTime: req.query.endDateTime + endHour},res))
-        // .then((err, data) => {
-        // if (err) {
-        //     res.status(500).send(err);
-        // })
-    // .then(res.status(200).send())
-    // .on('finished', (data) => {
-    //     console.log(data)
-    // })
+    .then(data => JSON.parse(data))
     .then((data) =>  {
-        // console.log(data)
-        res.status(200).send(data)
+        let events = 
+        Obstruct({
+            events: 
+            Obstruct.array({
+                title: 'name',
+                start: 'dates.start.localDate',
+                url: true
+            })
+        },data._embedded)
+        return events.events
+    })
+    .then((events) => {
+        // console.log(events)
+        res.status(200).send(events)
     })
     .catch((err) => {
         console.log(err)
     })
-        // .pipe(res)
 }
 
 module.exports = getData;
