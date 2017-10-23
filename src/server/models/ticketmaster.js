8d8c6b1dda848c5a8d4513aa5999abb29adcdd86
goliveira-daniel <goliveira.daniel@gmail.com>
2017-10-23 16:16:27 -0700

    Created function to dinamically build URL string with params

--- src/server/models/ticketmaster.js
+++ src/server/models/ticketmaster.js
@@ -1,9 +1,26 @@
 // const dotenv = require('dotenv').config();
 // const fetch = require('node-fetch');
-const request = require('request');
 const config = require ('../config.js');
+const _ = require('lodash');
+const request = require('request');
 const rp = require('request-promise');
 
+const queryParams = {
+    city: 'vancouver', // limits the city only to Vancouver for test purposes
+    size: 100 //tries to fetch as much events as possible
+}
+
+function buildUrlStr(clientParams) {
+    let urlStr = `${config.ticketmasterApiUrl}`
+    if (typeof clientParams == 'object') _.merge(queryParams,clientParams) 
+    Object.keys(queryParams).forEach(key => {
+        urlStr += '&' + key + '=' + queryParams[key] 
+    });
+    return urlStr
+}
+
 module.exports = (req) => {
-    return rp(`${config.ticketmasterApiUrl}&startDateTime=${req.startDateTime}&endDateTime=${req.endDateTime}&city=vancouver`)
+    // let test = buildUrlStr(req);
+    // return rp(`${config.ticketmasterApiUrl}&startDateTime=${req.startDateTime}&endDateTime=${req.endDateTime}&city=vancouver`)
+    return rp(buildUrlStr(req))
 }
