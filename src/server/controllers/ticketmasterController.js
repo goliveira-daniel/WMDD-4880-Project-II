259517b26f5c8b5594a58f43c6ae5207b0fddc69
goliveira-daniel <goliveira.daniel@gmail.com>
2017-10-31 01:11:09 -0700

    Removed obstruction function and dependency

--- src/server/controllers/ticketmasterController.js
+++ src/server/controllers/ticketmasterController.js
@@ -4,20 +4,27 @@ const Obstruct = require('obstruction')
 const startHour = 'T00:00:00Z'
 const endHour = 'T23:59:59Z'
 
+// TODO: Find how to iterate over an array using obstruction and 
 function getData(req, res) {
     model({ startDateTime: req.query.startDateTime + startHour, endDateTime: req.query.endDateTime + endHour})
     .then(data => JSON.parse(data))
     .then((data) =>  {
-        let events = 
-        Obstruct({
-            events: 
-            Obstruct.array({
-                title: 'name',
-                start: 'dates.start.localDate',
-                url: true
-            })
-        },data._embedded)
-        return events.events
+        let events = data._embedded.events.map(event => ({
+            title: event.name,
+            url: event.url,
+            start: event.dates.start.localDate,
+            classification: event.classifications[0].segment.name
+        }))
+        // Obstruct({
+        //     events: 
+        //     Obstruct.array({
+        //         title: 'name',
+        //         start: 'dates.start.localDate',
+        //         url: true
+        //         // classification: 'classifications[0].segment.name'
+        //     })
+        // },data._embedded)
+        return events
     })
     .then((events) => {
         // console.log(events)
